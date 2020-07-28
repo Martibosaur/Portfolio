0483a57488f33ad7c623aca1e3e9b8337afbe1eb
MartiboGit <44266609+MartiboGit@users.noreply.github.com>
2020-07-28 19:28:59 +1000

    Done

---
+++ build/precache-manifest.23ba581078e5eab410a98869fd178cf6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2070e853b4539cdacca509d468817b5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8d7f0187cf15e295665",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "509de6daeb4af9e99b7f",
+    "url": "/static/css/main.aa9423f6.chunk.css"
+  },
+  {
+    "revision": "e8d7f0187cf15e295665",
+    "url": "/static/js/2.2dae8c2e.chunk.js"
+  },
+  {
+    "revision": "ef3ec6e9ff89117c0a3a215fb445ba61",
+    "url": "/static/js/2.2dae8c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509de6daeb4af9e99b7f",
+    "url": "/static/js/main.6fdd7614.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7025dcbf0a3735febf97f1bbbbfb978e",
+    "url": "/static/media/astronaut.7025dcbf.svg"
+  },
+  {
+    "revision": "0567bb2787d0f50492238b5b98008370",
+    "url": "/static/media/docker.0567bb27.svg"
+  },
+  {
+    "revision": "5611e7e824f8f1e3cc8ab9d6cc52d49a",
+    "url": "/static/media/planet.5611e7e8.svg"
+  }
+]);
